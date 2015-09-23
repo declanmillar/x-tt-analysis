c18dcf43a6b5fabc96700bb5e517e9a50afff5ec
Declan Millar <d.millar@soton.ac.uk>
2015-09-23 11:36:04 +0100

    Removed generation folder.

--- Generation/Source/scattering.f
+++
@@ -1,9 +0,0 @@
-module scattering
-
-  implicit none
-
-  real :: fac_ee, fac_emu, fac_eq, fac_qq
-  real :: sigma
-  real :: sigma_pol(-1:1,-1:1,20), error_pol(-1:1,-1:1,20)
-
-end module scattering
