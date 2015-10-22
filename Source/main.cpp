9482259abc642627a3058280f8ec69099852e9d4
Declan Millar <d.millar@soton.ac.uk>
2015-10-22 11:31:16 +0100

    Deleted overlay.cpp.

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,11 +5,9 @@ int main(int argc, char* argv[])
   AtlasROOTStyle atlasStyle;
   atlasStyle.SetStyle();
 
-  TString channel("bbllnn"), model("GSM-SM"), options("_xc_");
-  int energy = 13; //TeV
-  int it = 5, points = 2000000;
+  const TString channel("bbllnn"), model("GSM-SM"), options("_xc_");
+  const int energy = 13, it = 5, points = 2000000, btags = 2;
   const double luminosity = 300000;
-  const int btags = 2;
   const bool discardComplex(false);
 
  AnalysisZprime analysis(channel, model, energy, options, it, points, luminosity, btags, discardComplex);
