775892f254a94acb5d84942dca6f410298e1ac7f
Declan Millar <declan.millar@cern.ch>
2016-09-01 12:48:37 +0100

    Removed Binary dir.

--- analyse.py
+++ analyse.py
@@ -4,12 +4,16 @@ import os, StringIO, re, optparse, subprocess, sys, glob, socket, string
 
 handler_name = "analyse.sh"
 executable = "analysis"
-queue = "8nh"
-walltime = "60:00:00"
-hostname = socket.gethostname()
+queue = "1nh"
+walltime = "01:00:00"
 run_directory = "."
 data_directory = "."
 
+
+# set directories
+hostname = socket.gethostname()
+run_directory = "."
+data_directory = "."
 if "lxplus" in hostname:
     run_directory = "/afs/cern.ch/user/d/demillar/zprime-top-analysis"
     data_directory = "/afs/cern.ch/work/d/demillar/zprime"
@@ -18,25 +22,26 @@ elif "cyan" in hostname:
     data_directory = "/scratch/dam1g09/zprime"
 else:
     sys.exit("Unrecognised hostname.")
-
 if os.path.isdir(data_directory) is False:
     sys.exit("The target data directory '%s' does not exist" % data_directory)
 
+# print handler
 handler = StringIO.StringIO()
 if "lxplus" in hostname:
     print >> handler, "export LD_LIBRARY_PATH=/afs/cern.ch/user/d/demillar/.RootTuple:$LD_LIBRARY_PATH"
     print >> handler, "source /afs/cern.ch/sw/lcg/external/gcc/4.8/x86_64-slc6/setup.sh"
     print >> handler, "cd %s" % run_directory
-    print >> handler, '%s/Binary/%s' % (run_directory, executable)
+    print >> handler, '%s/%s' % (run_directory, executable)
 elif "cyan" in hostname:
     print >> handler, "#!/bin/bash"
     print >> handler, "module load gcc/4.8.1; source /local/software/cern/root_v5.34.14/bin/thisroot.sh"
     print >> handler, "export LD_LIBRARY_PATH=/home/dam1g09/.RootTuple:$LD_LIBRARY_PATH"
     print >> handler, "cd %s" % run_directory
-    print >> handler, '%s/Binary/%s' % (run_directory, executable)
+    print >> handler, '%s/%s' % (run_directory, executable)
 else:
     sys.exit("Unrecognised hostname. No handler file created.")
 
+# write handler
 try:
     with open('%s' % handler_name, 'w') as handler_file:
         handler_file.write(handler.getvalue())
@@ -44,6 +49,7 @@ try:
 except IOERROR:
     sys.exit("ERROR! Cannot write handler file.")
 
+# run command
 subprocess.call("chmod a+x %s" % handler_name, shell = True)
 print "Submitting batch job."
 if "lxplus" in hostname: subprocess.call('bsub -q %s %s/%s' % (queue, run_directory, handler_name), shell = True)
