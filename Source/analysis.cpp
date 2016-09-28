f850e082e27a00cb6e3a43cae83605656bb825d5
Declan Millar <declan.millar@cern.ch>
2016-09-28 15:46:43 +0100

    Changed E range.

--- Source/analysis.cpp
+++ Source/analysis.cpp
@@ -479,8 +479,8 @@ void AnalysisZprime::AsymmetryUncertainty(TH1D* hA, TH1D* h1, TH1D* h2) {
 void AnalysisZprime::CreateHistograms() {
 
     double binWidth = 0.05;
-    double Emin = 0;
-    double Emax = 13;
+    double Emin = 2.025;
+    double Emax = 2.975;
     double nbins = (Emax - Emin)/binWidth;
 
     h_mtt = new TH1D("mtt", "m_{tt}", nbins, Emin, Emax);
