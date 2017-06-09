bad75802d31f9f1ac36b7a8fb135fc4bbc2938d2
Declan Millar <declan.millar@cern.ch>
2017-06-09 16:30:59 +0100

    spacing

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -328,7 +328,7 @@ void Analysis::SetupInputFiles()
                 if (i->path().extension() == ".root") {
                     nfiles++;
                     m_inputFiles->push_back(m_dataDirectory + "/" + i->path().filename().string());
-                    std::cout << "Input " << nfiles << ":         " << i->path().filename().string() << "\n";
+                    std::cout << "Input " << nfiles << ":        " << i->path().filename().string() << "\n";
                 }
                 if (i->path().extension() == ".log") m_weightFiles->push_back(m_dataDirectory + "/" + i->path().filename().string());
             }
