741eba99d9492af8f7360ad16ecea53574d912ca
Declan Millar <declan.millar@cern.ch>
2017-06-23 21:13:08 +0100

    get parameter

---
+++ include/get-parameter.hpp
@@ -0,0 +1,11 @@
+#ifndef _GET_PARAMETER_H_
+#define _GET_PARAMETER_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+double get_parameter(std::ifstream*);
+
+#endif
---
+++ src/get-parameter.cpp
@@ -0,0 +1,16 @@
+#include "get-parameter.hpp"
+
+double get_parameter(std::ifstream* file) {
+
+    if ( file->is_open() ) {
+        std::string temp;
+        getline(*file, temp);
+        size_t pos = temp.find('!');
+        std::istringstream read( pos == std::string::npos ? temp : temp.substr(0, pos) );
+        std::string line = read.str();
+        double parameter = std::stod(line);
+        return parameter;
+    }
+
+    throw std::invalid_argument("file");
+}
