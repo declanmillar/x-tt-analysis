17a7666148ac45c46f73ae885118a9b23a42a799
declan-millar <declan.millar@cern.ch>
2017-10-02 14:55:53 +0100

    run analysis in parallel using shell script

--- analyse.py
+++ analyse.py
@@ -4,8 +4,32 @@ import os, StringIO, subprocess, sys, socket
 
 # handler_name = sys.argv[1] + ".sh"
 executable = "analysis"
-walltime = "60:00:00"
-queue = "1nw"
+# walltime = "60:00:00"
+# queue = "1nw"
+
+parser = argparse.ArgumentParser(description="generate ttbar events")
+parser.add_argument("-a", "--arguments", help = "argument list to be passed to C++ program", default = "")
+parser.add_argument("-w", "--walltime", help = "cut off time for job completion", default = "60:00:00")
+parser.add_argument("-q", "--queue", help = "queue to join", default = "1nw")
+parser.add_argument("-p", "--parallel", help = "submit each input file as an individual job", default = True,  action = "store_false")
+parser.add_argument("-m" "model",          default = "SM")
+parser.add_argument("-g" "gg",             default = False, action = "store_true")
+parser.add_argument("-q" "qq",             default = False, action = "store_true")
+parser.add_argument("-u" "uu",             default = False, action = "store_true")
+parser.add_argument("-d" "dd",             default = False, action = "store_true")
+parser.add_argument("-f" "final_state",    default = "tt-bbeevv")
+parser.add_argument("-G" "add_gg",         default = False, action = "store_true")
+parser.add_argument("-Q" "add_qq",         default = False, action = "store_true")
+parser.add_argument("-E" "energy",         default = 13)
+parser.add_argument("-L" "luminosity",     default = -1)
+parser.add_argument("-b" "minimumBtags",   default = 2)
+parser.add_argument("-o" "options",        default = "")
+parser.add_argument("-r" "reconstruction", default = "TRN")
+parser.add_argument("-t" "tag",            default = "")
+parser.add_argument("-s" "slice",          default = False, action = "store_true")
+parser.add_argument("-i" "inputfilename",  default = "")
+parser.add_argument("-p" "processfilename",default = "")
+args = parser.parse_args()
 
 # set directories
 hostname = socket.gethostname()
@@ -26,16 +50,8 @@ if not os.path.isdir(run_directory):
 if not os.path.isdir(data_directory):
     sys.exit("Error: No directory %s" % data_directory)
 
-# collect command line arguments
-argstring = ""
-iterarg = iter(sys.argv)
-next(iterarg)
-filename = next(iterarg)
-for arg in iterarg:
-    argstring = argstring + " " + arg
-
-handler_name = filename + ".sh"
-logfile = filename + ".log"
+handler_name = args.inputfilename + ".sh"
+logfile = args.inputfilename + ".log"
 
 # print handler
 handler = StringIO.StringIO()
@@ -43,13 +59,13 @@ print >> handler, "#!/bin/bash"
 if "lxplus" in hostname:
     print >> handler, "source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6/setup.sh"
     print >> handler, "cd %s" % run_directory
-    print >> handler, "%s/%s %s" % (run_directory, executable, argstring)
+    print >> handler, "%s/%s %s" % (run_directory, executable, args.arguments)
 if "cyan" or "blue" in hostname:
     print >> handler, "source /home/dam1g09/.bash_profile"
     print >> handler, "echo 'changing to run directory ...'"
     print >> handler, "cd %s" % run_directory
     print >> handler, "echo 'running code ...'"
-    print >> handler, "%s/%s %s > %s/%s" % (run_directory, executable, argstring, run_directory, logfile)
+    print >> handler, "%s/%s %s > %s/%s" % (run_directory, executable, args.arguments, run_directory, logfile)
 else:
     sys.exit("Error: Unrecognised hostname")
 
