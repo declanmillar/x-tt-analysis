55753433947c4b713370e1b41877dadfd0d39d6c
declan-millar <declan.millar@cern.ch>
2017-10-23 14:37:38 +0100

    auto-indented neutrino-weighter

--- analyse.sh
+++ analyse.sh
@@ -1,10 +1,21 @@
 p="uu-AZX-tt-bbllvv_GLR-R-3_13TeV_CT14LL.txt"
 r="NuW"
 b="2"
-for f in $(ls -1 /scratch/dam1g09/zprime/uu-AZX-tt-bbemuvv_GLR-R-3_13TeV_CT14LL_???.lhef.gz)
+for f in $(ls -1 /scratch/dam1g09/zprime/uu-AZX-tt-bbemuvv_GLR-R-2.5_13TeV_CT14LL_???.lhef.gz)
 do
     c=$(echo $f | cut -d '/' -f 5)
     k=$(echo $c | cut -d '.' -f 1)
-    s="${k}_pythia_delphes_${r}_b${b}"
     ./analyse.py "${k}_pythia_delphes_${r}_b${b}" -i "${k}_pythia_delphes.root" -p "$p" -r "$r" -b "$b"
+done
+
+p="dd-AZX-tt-bbllvv_GLR-R-2.5_13TeV_CT14LL.txt"
+r="NuW"
+b="1"
+for f in $(ls -1 /scratch/dam1g09/zprime/dd-AZX-tt-bbeevv_GLR-R-2.5_13TeV_CT14LL_???.lhef.gz)
+do
+    c=$(echo $f | cut -d '/' -f 5)
+    k=$(echo $c | cut -d '.' -f 1)
+    l=$(echo $c | cut -d '.' -f 2)
+    m=$(echo $k.$l)
+    ./analyse.py "${m}_pythia_delphes_${r}_b${b}" -i "${m}_pythia_delphes.root" -p "$p" -r "$r" -b "$b"
 done
