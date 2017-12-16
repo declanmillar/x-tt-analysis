868421dfb4eab0c47cbef4dd2f3ed2044b087727
declan-millar <declan.millar@cern.ch>
2017-12-16 17:01:49 +0000

    use more smearing points in Neutrino reconstruction

--- src/neutrino-weighter.cpp
+++ src/neutrino-weighter.cpp
@@ -133,7 +133,7 @@ NeutrinoWeighter::NeutrinoWeighter( int setting, int event_number, double bmass 
     }
 
     ///-- Do Top Mass Smearing --///
-    // m_top_smears.push_back(170.5);
+    m_top_smears.push_back(170.5);
     m_top_smears.push_back(171.0);
     m_top_smears.push_back(171.5);
     m_top_smears.push_back(172.0);
@@ -141,12 +141,12 @@ NeutrinoWeighter::NeutrinoWeighter( int setting, int event_number, double bmass 
     m_top_smears.push_back(173.0);
     m_top_smears.push_back(173.5);
     m_top_smears.push_back(174.0);
-    // m_top_smears.push_back(174.5);
+    m_top_smears.push_back(174.5);
 
     // m_W_smears.push_back(80.3);
-    // m_W_smears.push_back(80.35);
+    m_W_smears.push_back(80.35);
     m_W_smears.push_back(80.4);
-    // m_W_smears.push_back(80.45);
+    m_W_smears.push_back(80.45);
     // m_W_smears.push_back(80.5);
 
     ///-- Crystal Ball Setup --///
