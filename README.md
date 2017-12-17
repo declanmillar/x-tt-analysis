c434915762f7357c0ce074096af88edea55d7319
declan-millar <declan.millar@cern.ch>
2017-12-17 23:22:50 +0000

    switching to cmake build; trying to add delphes submodule

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Apsis
 
-Previously know as `zprime-top-delphes-analysis`.
-
-Code to analyse top quark events in models containing *Z'* bosons. Reads `.root` output file from [Delphes](https://cp3.irmp.ucl.ac.be/projects/delphes).
+Code to analyse top pair production events  [Delphes](https://cp3.irmp.ucl.ac.be/projects/delphes).
 
 The input is stored in a `std::vector` of `std::tuple` with number of entries equal to the number of separate event files: `{string event_file, int proc_id}`.
 
 While an additional `std::vector` of `std::tuple` is created for each individual subprocess: `{string proc_file, int proc_id, int nfiles, double cross_section, double uncertainty, double weight}`, where `nfiles` is the number of input files for the process with `proc_id`.
+
+
+## Installation
+
+Dependencies: `boost`, `delphes`
 
 ## Important files
 
---
+++ build/CMakeFiles/analysis.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/declan/Projects/apsis/src/analysis.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/analysis.cpp.o"
+  "/Users/declan/Projects/apsis/src/atlas-style.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/atlas-style.cpp.o"
+  "/Users/declan/Projects/apsis/src/bool-to-string.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/bool-to-string.cpp.o"
+  "/Users/declan/Projects/apsis/src/get-parameter.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/get-parameter.cpp.o"
+  "/Users/declan/Projects/apsis/src/highest-pt.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/highest-pt.cpp.o"
+  "/Users/declan/Projects/apsis/src/kinematic-reconstructer.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/kinematic-reconstructer.cpp.o"
+  "/Users/declan/Projects/apsis/src/main.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/main.cpp.o"
+  "/Users/declan/Projects/apsis/src/match-bjets-to-leps.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/match-bjets-to-leps.cpp.o"
+  "/Users/declan/Projects/apsis/src/neutrino-weighter.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/neutrino-weighter.cpp.o"
+  "/Users/declan/Projects/apsis/src/progress-bar.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/progress-bar.cpp.o"
+  "/Users/declan/Projects/apsis/src/semilepton-reconstructer.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/semilepton-reconstructer.cpp.o"
+  "/Users/declan/Projects/apsis/src/solve-poly.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/solve-poly.cpp.o"
+  "/Users/declan/Projects/apsis/src/solve-quartic.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/solve-quartic.cpp.o"
+  "/Users/declan/Projects/apsis/src/trim.cpp" "/Users/declan/Projects/apsis/build/CMakeFiles/analysis.dir/src/trim.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "R__HAVE_CONFIG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/root"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
