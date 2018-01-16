c98bd224cbf753176aa187a757f2e2134bf6b1a7
declan-millar <declan.millar@cern.ch>
2018-01-16 12:05:56 +0000

    2 min run time; 100 GeV bin width

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -994,7 +994,7 @@ void Analysis::AsymmetryUncertainty(TH1D* hA, TH1D* h1, TH1D* h2)
 
 void Analysis::MakeHistograms()
 {
-    double binWidth = 0.01;
+    double binWidth = 0.1;
     double Emin = 0.05;
     double Emax = 12.95;
     double nbins = (Emax - Emin) / binWidth;
