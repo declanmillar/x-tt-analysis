fba3fa80830e68fcdb7c9d924a44ad7e31f310dc
declan-millar <declan.millar@cern.ch>
2018-02-12 12:12:57 +0000

    50 GeV bin width

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -994,11 +994,9 @@ void Analysis::AsymmetryUncertainty(TH1D* hA, TH1D* h1, TH1D* h2)
 
 void Analysis::MakeHistograms()
 {
-    double binWidth = 0.25;
-    // double Emin = 0.0;
-    double Emin = 0.05;
-    // double Emax = 13.0;
-    double Emax = 12.95;
+    double binWidth = 0.05;
+    double Emin = 0.025;
+    double Emax = 12.975;
     double nbins = (Emax - Emin) / binWidth;
     cout << "Plotting range:   " << Emin << " - " << Emax << " [TeV]\n";
     
