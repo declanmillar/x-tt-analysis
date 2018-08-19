fab48c8f50e90b0e11d5610783e39016d770f2b4
declan-millar <declan.millar@cern.ch>
2018-08-20 00:05:59 +0100

    2 hr walltime

--- analyse.py
+++ analyse.py
@@ -4,8 +4,8 @@ import os, StringIO, subprocess, sys, socket
 
 # handler_name = sys.argv[1] + ".sh"
 executable = "analysis"
-# walltime = "02:00:00" # NuW
-walltime = "00:01:00" # KIN
+walltime = "02:00:00" # NuW
+# walltime = "00:01:00" # KIN
 queue = "1nw"
 nodes = "1"
 ppn = "1"
